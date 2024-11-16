4ad70947771cbebf9068c3599734488ecf3bfe67
VanessaSimanta <132068764+VanessaSimanta@users.noreply.github.com>
2024-11-16 15:35:42 +0700

    Revert "Sign Up and login"
    This reverts commit a60e817c6864e743a668cebab352e0c99f8b7f97.

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// POST request untuk sign-up
-router.post('/signup', userController.signup);
-
-module.exports = router;
--- frontend/app/controllers/loginCtrl.js
+++
@@ -1,5 +0,0 @@
-classApp.controller('loginCtrl', function($scope, $location) {
-    $scope.signup = function() {
-        $location.path('/signUp'); 
-    };
-});
